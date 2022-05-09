35ff9329abef3e92f1b1384d532f176a2478c39b
sreemonta20 <sbhowmikcse08@gmail.com>
2022-05-09 23:41:00 +0600

    commit changes - sorted array (asc) improved - sorted array (desc) program done

--- cplusproblem/givenarraysort.cpp
+++ cplusproblem/givenarraysort.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-void sort(int arr[], int size){
+void sortasc(int arr[], int size){
     int temp = 0;
 
     for(int i=0; i<size; i++){
@@ -10,23 +10,71 @@ void sort(int arr[], int size){
                 temp = arr[i];
                 arr[i] = arr[i+1];
                 arr[i+1] = temp;
-                while((arr[i] < arr[i-1]) && i-1 >=0){
+                // while((arr[i] < arr[i-1]) && i-1 >=0){
+                //     temp = 0;
+                //     temp = arr[i];
+                //     arr[i] = arr[i-1];
+                //     arr[i-1] =  temp;    
+                //     i--;     
+                // }
+                 while((arr[i] < arr[i-1]) && i >=0){
                     temp = 0;
                     temp = arr[i];
                     arr[i] = arr[i-1];
-                    arr[i-1] =  temp;         
+                    arr[i-1] =  temp;    
+                    i--;     
                 }
             }
         //}
     }
     //return 0;
 }
+
+void sortdesc(int arr[], int size){
+    int temp = 0;
+
+    for(int i =0; i<size; i++){
+        if((arr[i] < arr[i+1]) && (i>=0) && ((i+1) < size)){
+            temp = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = temp;
+
+            while((arr[i-1] < arr[i]) && i>=0){
+                temp = 0;
+                temp = arr[i-1];
+                arr[i-1] = arr[i];
+                arr[i] = temp;
+                i--;
+            }
+        }
+    }
+}
 int main(){
-    int arr[5] = {4, 1, 3, 2, 5};
+    // int arr[6] = {4, 1, 3, 6, 2, 5};
+    int arr[6] = {6,3,5,4, 2,1};
+    int arrdesc[6] = {1,2,3,4,5,6};
     int * p =  arr;
-    sort(arr, 5);
-    for(int i =0; i< 5; i++){
+    int * q = arrdesc;
+    cout <<"Given array (for asc) is :"<<"\t\n";
+    for(int i =0; i< 6; i++){
+        cout <<"Array value at index "<<i<<" is "<<arr[i]<<"\t";
+    }
+    cout <<"\t\n";
+    cout <<"Sorted array(Asc) is :"<<"\t\n";
+    sortasc(arr, 6);
+    for(int i =0; i< 6; i++){
         cout <<"Array value at "<<i<<" is "<<p[i]<<"\t\n";
+    }
+
+    cout <<"Given array (for desc) is :"<<"\t\n";
+    for(int i =0; i< 6; i++){
+        cout <<"Array value at index "<<i<<" is "<<arrdesc[i]<<"\t";
+    }
+    cout <<"\t\n";
+    cout <<"Sorted array(desc) is :"<<"\t\n";
+    sortdesc(arrdesc, 6);
+    for(int i =0; i< 6; i++){
+        cout <<"Array value at "<<i<<" is "<<q[i]<<"\t\n";
     }
 
     return 0;
