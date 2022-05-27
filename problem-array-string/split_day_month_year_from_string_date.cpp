baeac8f36711e1aa2c33d2650690ca8f5f5937e4
sreemonta20 <sbhowmikcse08@gmail.com>
2022-05-27 09:12:51 +0600

    commit changes - initial solution (working)

---
+++ problem-array-string/split_day_month_year_from_string_date.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+
+    string date= "21/9/2013"; int size=0, day=0,month=0,year=0;
+    
+    for(int i = 0; date[i] != '\0'; i++){
+        size +=1;
+    }
+
+    for(int i=0; i <size; i++){
+        if((ctoi(date[i])) && (date[i+2] == "/")){
+            day = ctoi(date[i]+date[i+1]);
+        }else if((date[i-1] == "/") && date[i+1] == "/"){
+            month = ctoi(date[i]);
+        }else if(date[i+1] == '\0'){
+            year = ctoi(date[i-3]+date[i-2]+date[i-1]+date[i]);
+        }
+    }
+
+    cout<<"Day, month, and year are "<<day<<month<<year<<"\n";
+
+return 0;
+}
